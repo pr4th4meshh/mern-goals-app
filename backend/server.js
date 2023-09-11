6acc3e344e02ee735a50d0c72ea2703d8471e5aa
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2023-09-11 20:47:08 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+const express = require("express")
+const colors = require('colors')
+const dotenv = require("dotenv").config()
+const port = process.env.PORT || 5000
+const connectDb = require('./config/db')
+const { errorHandler } = require("./middleware/errorMiddleware")
+
+connectDb()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use("/api/goals", require("./routes/goalRoutes"))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
